4162f82ef6e5f1fd02bc0885aece82953fee2257
dsmi235 <dustinsmith235@gmail.com>
2025-06-17 10:57:15 -0400

    create player class, draw player in main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
-
+from player import *
 
 
 def main():
@@ -12,12 +12,17 @@ def main():
 	print("Screen width:", SCREEN_WIDTH)
 	print("Screen height:", SCREEN_HEIGHT)
 
+	game_clock= pygame.time.Clock()
+	dt = 0
+
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	
+	player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT /2))
 	while pygame.get_init():
 		screen.fill("black")
-		pygame.display.flip()
+		player.draw(screen)
 		
+		pygame.display.flip()
+		dt = game_clock.tick(60) / 1000 
 		for event in pygame.event.get():
    			 if event.type == pygame.QUIT:
        				 return
