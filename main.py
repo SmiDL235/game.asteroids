0eec2adf5f0864e3f26210eca92757ab2184559b
dsmi235 <dustinsmith235@gmail.com>
2025-06-17 12:42:15 -0400

    player move, playr containers

--- main.py
+++ main.py
@@ -14,15 +14,21 @@ def main():
 
 	game_clock= pygame.time.Clock()
 	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT /2))
 	while pygame.get_init():
+		dt = game_clock.tick(60) / 1000 
+		updatable.update(dt)
 		screen.fill("black")
-		player.draw(screen)
+		for player in drawable:
+			player.draw(screen)
 		
 		pygame.display.flip()
-		dt = game_clock.tick(60) / 1000 
+		 
 		for event in pygame.event.get():
    			 if event.type == pygame.QUIT:
        				 return
