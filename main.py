d61c66456d3b7e03b555f89c0fe149f299694e0a
dsmi235 <dustinsmith235@gmail.com>
2025-06-17 17:21:20 -0400

    collison check, Game Over generated

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import *
-
+from asteroid import *
+from asteroidfield import *
 
 def main():
 	pygame.init()
@@ -14,24 +16,37 @@ def main():
 
 	game_clock= pygame.time.Clock()
 	dt = 0
+	asteroids = pygame.sprite.Group()
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	Player.containers = (updatable, drawable)
-
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable,)
+	
+	asteroidfield = AsteroidField()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT /2))
 	while pygame.get_init():
-		dt = game_clock.tick(60) / 1000 
+		dt = game_clock.tick(60) / 1000
 		updatable.update(dt)
-		screen.fill("black")
-		for player in drawable:
-			player.draw(screen)
 		
+		screen.fill("black")
+		
+
+		for asteroid in asteroids:
+			
+			if  asteroid.collision_check(player):
+				print("Game over!")
+				sys.exit()
+		
+		for entity in drawable:
+			entity.draw(screen)
+
 		pygame.display.flip()
-		 
+
 		for event in pygame.event.get():
    			 if event.type == pygame.QUIT:
        				 return
-	
+
 if __name__ == "__main__":
     main()
